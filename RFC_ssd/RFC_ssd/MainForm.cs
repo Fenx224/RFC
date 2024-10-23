a7187dfc8a6d94a946a33831af7fed672392650a
Fenx <zagj080422hgtvrsa8@cbtis217.edu.mx>
2024-10-23 10:12:18 -0600

    Agregamos comentarios al codigo

--- RFC_ssd/RFC_ssd/MainForm.cs
+++ RFC_ssd/RFC_ssd/MainForm.cs
@@ -29,15 +29,21 @@ namespace RFC_ssd
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		
+		//Definimos las variables donde almacenaremos lo que el usuario introdujo//
 		string fecha="",nombre="",apellidop="",apellidom="";
 		void BTNcalcularClick(object sender, EventArgs e)
 		{
+			//Introducimos lo que el usuario introdujo en las variables//
 			fecha=DT_Calendario.Text;
 			nombre=TXTnombre.Text;
 			apellidop=TXTapellidop.Text;
 			apellidom=TXTapellidoma.Text;
+			
+			//Creamos una condicion para saber si se llenaron todos los campos/
 			if(TXTnombre.Text != "" && TXTapellidop.Text != "" && TXTapellidoma.Text != "" && TXTDireccion.Text != "" && TXTtelefono.Text != "")
 			{
+				//Utilizamos substrings para almacenar los datos que necesitamos//
 				string fecha1=fecha.Substring(0,2);
 				string fecha2=fecha.Substring(3,2);
 				string fecha3=fecha.Substring(8);
@@ -45,8 +51,12 @@ namespace RFC_ssd
 				string paterno=apellidop.Substring(0,2);
 				string materno=apellidom.Substring(0,1);
 				string nc=paterno+materno+name;
+				
+				//Imprimimos La RFC creada//
 				LBLrfc.Text=nc.ToUpper()+fecha3+fecha2+fecha1;
 			}
+			
+			//Si la condicion no se cumplio mostramos un mensaje indicando que debe llenarlos//
 			else
 			{
 				MessageBox.Show("Debes de llenar todos los campos");
